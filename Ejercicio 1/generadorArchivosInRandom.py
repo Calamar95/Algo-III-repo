8d0a62d036d686bf18be455065f204bdab0d7fbf
Candelaria Campos <candelaria.campos@mercadolibre.com>
2015-08-23 20:23:27 -0300

    AHORA SI - un ejemplo del ej1 con su grafico y los archivos usados

---
+++ Ejercicio 1/generadorArchivosInRandom.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+import random
+
+file = open('ejemploConNumerosRandom.in', 'w+')
+file2 = open('ejemploTamCiudades.txt', 'w+')
+
+for x in xrange(1,10000, 100):
+	i = 0
+	ciudad = 0
+	file.write(str(random.randrange(x)) + '\n')
+	file2.write(str(x) + '\n')
+
+	while i < x:
+		ciudad = ciudad + (random.randrange(i+1)+1)
+		file.write(str(ciudad) + ' ')
+		i = i + 1
+	
+	file.write('\n')
+
+file2.close()
