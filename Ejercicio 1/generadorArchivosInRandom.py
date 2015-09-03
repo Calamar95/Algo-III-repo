3239a04a37ba73ca4f24b703437be955827a4d14
Candelaria Campos <candelaria.campos@mercadolibre.com>
2015-09-03 00:20:07 -0300

    3er grafico con sus respectivos archivos, agregado a latex, falta explicarlos

--- Ejercicio 1/generadorArchivosInRandom.py
+++ Ejercicio 1/generadorArchivosInRandom.py
@@ -19,13 +19,32 @@ import random
 
 #file2.close()
 
-file = open('2doEjemploCableCorto.in', 'w+')
+#file = open('2doEjemploCableCorto.in', 'w+')
+#file2 = open('ejemploTamCiudades.txt', 'w+')
+
+#for x in xrange(1,10000, 100):
+#	i = 0
+#	ciudad = 0
+#	file.write('1' + '\n')
+#	file2.write(str(x) + '\n')
+
+#	while i < x:
+#		ciudad = ciudad + (random.randrange(i+1)+1)
+#		file.write(str(ciudad) + ' ')
+#		i = i + 1
+	
+#	file.write('\n')
+
+#file2.close()
+
+
+file = open('3erEjemploCableLargo.in', 'w+')
 file2 = open('ejemploTamCiudades.txt', 'w+')
 
 for x in xrange(1,10000, 100):
 	i = 0
 	ciudad = 0
-	file.write('1' + '\n')
+	file.write('100000000' + '\n')
 	file2.write(str(x) + '\n')
 
 	while i < x:
