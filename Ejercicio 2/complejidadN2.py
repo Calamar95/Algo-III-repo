d54c5fbd040672a966528582fe9f7d14af60d404
lew axel ariel <alew@ws18.labo4>
2015-09-04 13:03:30 -0300

    mas modificaciones

--- Ejercicio 2/complejidadN2.py
+++ Ejercicio 2/complejidadN2.py
@@ -6,5 +6,5 @@ from math import factorial
 file = open('complejidadN2.txt', 'w+')
 
 for x in xrange(1, 20, 1):
-		file.write(str(factorial(x)) + ' ' +'\n')
+		file.write(str(float(factorial(x))/3) + ' ' +'\n')
 file.close()
